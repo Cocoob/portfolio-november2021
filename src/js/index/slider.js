07314099f46ab66924415de9f8568f12a047cb6e
cocoob <corentin.bousquet11@gmail.com>
2021-11-23 16:32:27 +0100

    Modif slider

--- src/js/index/slider.js
+++ src/js/index/slider.js
@@ -11,6 +11,14 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Sizes
+ */
+ const sizes = {
+    width: 626,
+    height: 417
+}
+
+/**
  * Object
  */
 // Texture
@@ -21,7 +29,7 @@ const occitanTexture2 = textureLoader.load('/textures/4.png')
 const occitanTexture3 = textureLoader.load('/textures/4.png')
 const occitanTexture4 = textureLoader.load('/textures/4.png')
 
-// gradientTexture.magFilter = THREE.NearestFilter
+// occitanTexture.magFilter = THREE.NearestFilter
 
 // Material
 const material = new THREE.MeshBasicMaterial(
@@ -33,17 +41,17 @@ const material = new THREE.MeshBasicMaterial(
 
 // Mesh
 const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(3, 2, 100),
+    new THREE.PlaneGeometry(1, 1, 100),
     material
 )
 
 const plane2 = new THREE.Mesh(
-    new THREE.PlaneGeometry(3, 2, 100),
+    new THREE.PlaneGeometry(1, 1, 100),
     material
 )
 
 const plane3 = new THREE.Mesh(
-    new THREE.PlaneGeometry(3, 2, 100),
+    new THREE.PlaneGeometry(1, 1, 100),
     material
 )
 
@@ -51,7 +59,7 @@ scene.add(plane, plane2, plane3)
 const mesh = [plane,plane2,plane3]
 
 // Distance
-const objectDistance = 6
+const objectDistance = 3
 plane2.position.x = objectDistance * 1
 plane3.position.x = objectDistance * 2
 
@@ -61,15 +69,33 @@ plane3.position.x = objectDistance * 2
 let count = 0
 let nbrSlide = mesh.length
 
-// Number
-const span = document.querySelector('.slide-number')
-span.innerHTML = `0${count+1} / 0${mesh.length}`
+// Slide number affichage
+const span = document.querySelector('.animate-text').children
+const spanLen = span.length
+
+function animateText(){
+    for ( let i = 0; i<spanLen; i++)
+    {
+        span[i].classList.remove('text-in')
+        span[i].classList.add('text-out')
+    }
+
+    span[count].classList.add('text-in')
+
+    if(span[count].classList.contains('text-in'))
+    {
+        span[count].classList.remove('text-out')
+    }
+}
+
+// span.innerHTML = `0${count+1} / 0${mesh.length}`
 
 
 // Animate camera
 // Suivant
 const next = document.querySelector('.s1')
 next.addEventListener('click', () => {
+
     if(count < nbrSlide - 1)
     {
         count++;
@@ -77,8 +103,8 @@ next.addEventListener('click', () => {
         gsap.to(
             camera.position,
             {
-                duration:1,
-                x: "+=" + 6
+                duration:0.5,
+                x: "+=" + 3
             }
         )
     }
@@ -87,13 +113,16 @@ next.addEventListener('click', () => {
         gsap.to(
             camera.position,
             {
-                duration:1,
+                duration:0.5,
                 x: 0
             }
         )
     }
 
-    span.innerHTML = `0${count+1} / 0${mesh.length}`
+    // Update slide number
+    animateText()
+    // span.innerHTML = `0${count+1} / 0${mesh.length}`
+
 })
 
 // Précédent
@@ -106,8 +135,8 @@ prev.addEventListener('click', () => {
         gsap.to(
             camera.position,
             {
-                duration:1,
-                x: "-=" + 6
+                duration:0.5,
+                x: "-=" + 3
             }
         )
     }
@@ -116,34 +145,26 @@ prev.addEventListener('click', () => {
         gsap.to(
             camera.position,
             {
-                duration:1,
-                x: 12
+                duration:0.5,
+                x: 6
             }
         )
     }
 
-    span.innerHTML = `0${count+1} / 0${mesh.length}`
+    // Update slide number
+    // span.innerHTML = `0${count+1} / 0${mesh.length}`
 })
-
 
 /**
  * Lights
  */
 
 
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-
 window.addEventListener('resize', () =>
 {
     // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+    sizes.width = 626
+    sizes.height = 417
 
     // Update camera
     camera.aspect = sizes.width / sizes.height
@@ -159,8 +180,22 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 6
+camera.position.z = 2
 scene.add(camera)
+
+const dist = camera.position.z
+const height = 1
+camera.fov = 2*(180/Math.PI)*Math.atan(height/(2*dist))
+
+if(sizes.width/sizes.height>1){
+    plane.scale.x = camera.aspect
+    plane2.scale.x = camera.aspect
+    plane3.scale.x = camera.aspect
+} else {
+    plane.scale.y = 1 / camera.aspect
+    plane2.scale.y = 1 /camera.aspect
+    plane3.scale.y = 1 /camera.aspect
+}
 
 
 /**
